74a6e0ed6d2cebf3ba484bac7b94ef42c9580eec
JinCheng Sun <1962945668@qq.com>
2023-10-19 15:14:05 +0800

    实现C++画图尝试
    初步使用matplotlibcpp

---
+++ README.md
@@ -0,0 +1,7 @@
+## 环境
+'''
+bash
+sudo apt-get install python3-matplotlib python3-numpy 
+'''
+## 用法
+只需要配合cmakelists.txt以及matplotlibcpp.h即可使用
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sun/Documents/demo_plot
+# Build directory: /home/sun/Documents/demo_plot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
